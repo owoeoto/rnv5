eff1ecd23da5270945ae9cc550d031c1bb3cb709
Ray <ohmyray@163.com>
2021-08-31 21:54:15 +0800

    init project

---
+++ model/users.js
@@ -0,0 +1,9 @@
+export default Users = [
+    {
+        id: 1, 
+        email: 'user1@qq.com',
+        username: 'admin', 
+        password: 'password', 
+        userToken: 'tokenAdmin123'
+    }
+];
